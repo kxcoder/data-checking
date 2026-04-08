22c7e15995fd0b3df8d1af627923dc144bcb2d8c
kxcoder <ketao1989@gmail.com>
2026-04-09 00:30:52 +0800

    feat: add FeatureQueryServiceImpl Dubbo service

---
+++ data-checking-adapter/src/main/java/com/example/datachecking/adapter/dubbo/FeatureQueryServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.datachecking.adapter.dubbo;
+
+import com.example.datachecking.api.FeatureQueryService;
+import com.example.datachecking.api.dto.FeatureQueryRequest;
+import com.example.datachecking.api.dto.FeatureQueryResponse;
+import com.example.datachecking.application.service.FeatureQueryApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
+
+@Slf4j
+@DubboService(version = "1.0.0", group = "data-checking", timeout = 5000)
+@RequiredArgsConstructor
+public class FeatureQueryServiceImpl implements FeatureQueryService {
+
+    private final FeatureQueryApplicationService featureQueryApplicationService;
+
+    @Override
+    public FeatureQueryResponse queryFeatures(FeatureQueryRequest request) {
+        log.info("Feature query request: uid={}, keys={}", request.getUid(), request.getFeatureKeys());
+        return featureQueryApplicationService.queryFeatures(request);
+    }
+}
